042ed14501a9f77233cfa4baadfa3c584401b730
Skanda <skanda.td@gmail.com>
2025-05-14 14:28:59 +0530

    Spring init code

---
+++ src/main/java/com/example/nativelazy/NativelazyApplication.java
@@ -0,0 +1,13 @@
+package com.example.nativelazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativelazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativelazyApplication.class, args);
+	}
+
+}
